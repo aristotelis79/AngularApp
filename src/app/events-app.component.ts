759fcd529185a2e0d75a1682a3365ce4c351d4e3
aristotelis79 <aristotelis79@users.noreply.github.com>
2019-12-27 09:40:40 +0200

    working angular with bootstrap and scss styles

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>{{title}}<h1>',
-  styleUrls: ['./app.component.sass']
+  template: `
+    <h1>{{title}}</h1>
+    <img src = '/assets/images/basic-shield.png'/>
+  `,
+  styleUrls: ['./app.component.scss']
 })
 export class EventsAppComponent {
   title = 'angularApp';
