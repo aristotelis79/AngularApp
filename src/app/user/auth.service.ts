0ccf02f89a5e02b7c86ab58a8b9260e16622cb39
aristotelis <aristotelis79@gmail.com>
2021-05-17 08:32:59 +0300

    login with http

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,19 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {IUser} from './user.model';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { IUser } from './user.model';
+
 @Injectable()
 export class AuthService {
   curretUser: IUser;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  loginUser(userName: string, password: string ){
-    this.curretUser =
-    {
-      id: 1,
-      userName,
-      firstName: 'Aris',
-      lastName : 'Papas',
-    };
+  loginUser(userName: string, password: string ): Observable<IUser | any> {
+    // this.curretUser =
+    // {
+    //   id: 1,
+    //   userName,
+    //   firstName: 'Aris',
+    //   lastName : 'Papas',
+    // };
+    const options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+    const loginInfo = {username: userName, password};
+
+    return this.http.post('/api/login/', loginInfo, options)
+    .pipe(tap(data => {
+      this.curretUser = (data['user'] as IUser);
+    }))
+    .pipe(catchError(err => of(false)));
   }
 
   isAuthenticated(){
--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +12,22 @@ export class LoginComponent implements OnInit {
   userName: string;
   password: string;
   mouseoverLogin: boolean;
-
+  loginInvalid = false;
+  
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login(loginformValues){
-    this.authService.loginUser(loginformValues.userName, loginformValues.password);
-    this.router.navigate(['events']);
+    this.authService.loginUser(loginformValues.userName, loginformValues.password)
+    .subscribe(resp => {
+      if (!resp){
+        this.loginInvalid = true;
+      }else{
+        this.router.navigate(['events']);
+      }
+    });
   }
 
   cancel(){
